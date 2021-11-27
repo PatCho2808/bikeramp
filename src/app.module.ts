7093ae0c605b3f6f959d3ef77d3ad6cf857ffd18
Patrycja <patcho2808@gmail.com>
2021-11-27 10:09:32 +0100

    Added validation for environmental variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,22 @@ import { Trip } from './trips/trip.entity';
 import { TripsModule } from './trips/trips.module';
 import { StatsModule } from './stats/stats.module';
 import { SharedModule } from './shared/shared.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     TripsModule,
     ConfigModule.forRoot({
       load: [configuration],
+      validationSchema: Joi.object({
+        GOOGLE_API_KEY: Joi.string().required(),
+        DB_TYPE: Joi.valid('postgres', 'mysql').required(),
+        DB_HOST: Joi.string().hostname().required(),
+        DB_PORT: Joi.number().port().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+      }),
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
